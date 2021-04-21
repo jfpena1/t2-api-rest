4eefd41e1927b71a46d4b72e1cd54b6d7e14c8d0
jfpena1 <jfpena1@uc.cl>
2021-04-21 15:01:53 -0400

    modified max. length restrictions for urls in models

--- music/models.py
+++ music/models.py
@@ -3,37 +3,37 @@ from django.db import models
 
 class Artist(models.Model):
     id = models.CharField(max_length=22, primary_key=True)
-    name = models.CharField(max_length=50)
+    name = models.CharField(max_length=200)
     age = models.IntegerField()
-    albums = models.CharField(max_length=50, default="")
-    tracks = models.CharField(max_length=50, default="")
-    url = models.CharField(max_length=50,  default="")
+    albums = models.CharField(max_length=200, default="")
+    tracks = models.CharField(max_length=200, default="")
+    url = models.CharField(max_length=200, default="")
 
     def __str__(self):
         return self.name
 
 class Album(models.Model):
     id = models.CharField(max_length=22, primary_key=True)
-    name = models.CharField(max_length=50)
-    genre = models.CharField(max_length=50)
+    name = models.CharField(max_length=200)
+    genre = models.CharField(max_length=200)
     artist_id = models.ForeignKey(Artist, on_delete=models.CASCADE, default="")
-    artist = models.CharField(max_length=50) #url
-    tracks = models.CharField(max_length=50)
-    url = models.CharField(max_length=50)
+    artist = models.CharField(max_length=200, default="") #url
+    tracks = models.CharField(max_length=200, default="")
+    url = models.CharField(max_length=200, default="")
 
     def __str__(self):
         return self.name
 
 class Track(models.Model):
     id = models.CharField(max_length=22, primary_key=True)
-    name = models.CharField(max_length=50)
+    name = models.CharField(max_length=200)
     duration = models.IntegerField()
     times_played = models.IntegerField()
     artist_id = models.ForeignKey(Artist, on_delete=models.CASCADE, default="")
-    artist = models.CharField(max_length=50) #url
+    artist = models.CharField(max_length=200, default="") #url
     album_id = models.ForeignKey(Album, on_delete=models.CASCADE, default="")
-    album = models.CharField(max_length=50) #url
-    url = models.CharField(max_length=50)
+    album = models.CharField(max_length=200, default="") #url
+    url = models.CharField(max_length=200, default="")
 
     def __str__(self):
         return self.name
